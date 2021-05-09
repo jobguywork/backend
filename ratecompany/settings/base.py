699522843714f308940d4d2cd9f77fba93d5b29b
alirezainjast <allalirezamail@gmail.com>
2021-05-09 21:19:55 +0430

    translation

--- ratecompany/settings/base.py
+++ ratecompany/settings/base.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
