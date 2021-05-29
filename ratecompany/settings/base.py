91b017f26ab34ea861e417850a5c392409df7f23
mamsi <m.hossein95th@gmail.com>
2021-05-29 19:33:24 +0430

    django.contrib.gis added to the INSTALLED_APPS

--- ratecompany/settings/base.py
+++ ratecompany/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+    'django.contrib.gis',
     # second party
     'rest_framework',
     'rest_framework_swagger',
